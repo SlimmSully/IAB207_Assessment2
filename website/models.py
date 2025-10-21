20211b3b1c30331ace3eb3e94a4ae51cccd1ce49
FlueyA <tugulduramarjargal1@gmail.com>
2025-10-22 07:27:56 +1000

    models.py
    for auth

--- website/models.py
+++ website/models.py
@@ -3,6 +3,11 @@ from datetime import datetime
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
 
 class Event(db.Model):
 
