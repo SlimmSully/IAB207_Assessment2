09a7760c52bd8f7c2029ec108843faaad7a97050
Therealtoast2249 <harryghandy@gmail.com>
2025-10-28 03:03:08 +1000

    Add files via upload
    removed website from branch to upload my website with all files

---
+++ website/forms.py
@@ -0,0 +1,58 @@
+from flask_wtf import FlaskForm
+from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField, SelectField, IntegerField, FileField,RadioField,DateField,DateTimeField,TimeField
+from wtforms.validators import InputRequired, Length, Email, EqualTo, DataRequired, NumberRange
+
+# creates the login information
+class LoginForm(FlaskForm):
+    user_name=StringField("User Name", validators=[InputRequired('Enter user name')])
+    password=PasswordField("Password", validators=[InputRequired('Enter user password')])
+    submit = SubmitField("Login")
+
+ # this is the registration form
+class RegisterForm(FlaskForm):
+    user_name=StringField("User Name", validators=[InputRequired()])
+    email = StringField("Email Address", validators=[Email("Please enter a valid email")])
+    # linking two fields - password should be equal to data entered in confirm
+    password=PasswordField("Password", validators=[InputRequired(),
+                  EqualTo('confirm', message="Passwords should match")])
+    confirm = PasswordField("Confirm Password")
+
+    # submit button
+    submit = SubmitField("Register")
+
+# form for adding comments on event details page
+class CommentForm(FlaskForm):
+    content = TextAreaField('Write a comment', validators=[InputRequired(), Length(max=500)])
+    submit = SubmitField('Post Comment')
+
+# form for booking tickets
+class BookingForm(FlaskForm):
+    ticket_type = SelectField('Ticket Type', coerce=int, validators=[DataRequired()])
+    ticket_quantity = IntegerField(
+        'Number of Tickets',
+        validators=[DataRequired(), NumberRange(min=1, max=10)]
+    )
+    submit = SubmitField('Book Now')
+
+
+class EventForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired()])
+    genre = SelectField("Genre", choices=[
+        ("Hip Hop","Hip Hop"), ("R&B","R&B"), ("Jazz","Jazz"),
+        ("Rap","Rap"), ("Electronic","Electronic"), ("Comedy","Comedy")
+    ], validators=[DataRequired()])
+    description = TextAreaField("Description", validators=[DataRequired()])
+    location = StringField("Location", validators=[DataRequired()] )
+    event_date = DateField("Date", format="%Y-%m-%d", validators=[DataRequired()])
+    start_time = TimeField("Start time", format="%H:%M", validators=[DataRequired()])
+    end_time   = TimeField("End time", format="%H:%M")
+    img = FileField("Upload Image")
+    submit = SubmitField("Save event")
+
+class TicketForm(FlaskForm):
+    Label = StringField("Ticket Type", validators=[InputRequired()])
+    Price = IntegerField("Price", validators=[InputRequired()])
+    Quota = IntegerField("quota for profit")
+    
+
+    
