6e5f5eded754a3c21f6a53cbef20437dcfa163bf
PeachyyPeachh <133387752+PeachyyPeachh@users.noreply.github.com>
2025-10-28 14:39:19 +1000

    this has the most recent front end
    still working on front end but its up there

--- website/models.py
+++ website/models.py
@@ -4,12 +4,9 @@ from datetime import datetime
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(255), unique=True, nullable=False)
-    password_hash = db.Column(db.String(255), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+# USER MODEL
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
 
     user_id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(50), nullable=False)
@@ -106,4 +103,4 @@ class Booking(db.Model):
         return f'<Booking user={self.user_id}, ticket={self.ticket_type_id}, qty={self.quantity}>'
 
 
-class Order(db.Model):
+# class Order(db.Model):
