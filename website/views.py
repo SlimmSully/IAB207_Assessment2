cff6466fbae7b3e70c1b34c6a02597ffbd347bee
Therealtoast2249 <Harryghandy@gmail.com>
2025-10-31 02:34:29 +1000

    fixed various issues and error, added more genres and made it so logged in users can see edit button

--- website/views.py
+++ website/views.py
@@ -14,15 +14,16 @@ main_bp = Blueprint('main', __name__)
 def index():
     selected_genre = request.args.get('genre')
     genres = [g[0] for g in db.session.query(Event.genre).distinct().all()]
-    if selected_genre:
+
+    if selected_genre and selected_genre != "All":
         events = Event.query.filter_by(genre=selected_genre).all()
     else:
         events = Event.query.all()
-    carousel_events = Event.query.order_by(db.func.random()).limit(3).all()
-    return render_template('index.html', events=events, genres=genres,
-                        selected_genre=selected_genre, carousel_events=carousel_events)
 
-from flask import request
+    carousel_events = Event.query.order_by(db.func.random()).limit(3).all()
+
+    return render_template(
+        'index.html',events=events,genres=genres,selected_genre=selected_genre,carousel_events=carousel_events)
 
 @main_bp.route('/search')
 def search():
